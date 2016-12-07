bf73746fe231f7dae72892b207af38c73ea198e2
Prema Chand Alugu <premaca@gmail.com>
2016-12-08 03:55:34 +0530

    remove unsupported no-override animation control

--- services/core/java/com/android/server/wm/AppTransition.java
+++ services/core/java/com/android/server/wm/AppTransition.java
@@ -255,7 +255,6 @@ public class AppTransition implements Dump {
     private int[] mActivityAnimations = new int[10];
     private int mAnimationDuration;
     private boolean mIsResId = false;
-    private boolean mNoOverrides;
 
     private int mCurrentUserId = 0;
     private long mLastClipRevealTransitionDuration = DEFAULT_APP_TRANSITION_DURATION;
@@ -1806,7 +1805,6 @@ public class AppTransition implements Dump {
 
     void overridePendingAppTransition(String packageName, int enterAnim, int exitAnim,
             IRemoteCallback startedCallback) {
-        if (mNoOverrides) return;
         if (isTransitionSet()) {
             clear();
             mNextAppTransitionType = NEXT_TRANSIT_TYPE_CUSTOM;
@@ -1822,7 +1820,6 @@ public class AppTransition implements Dump {
 
     void overridePendingAppTransitionScaleUp(int startX, int startY, int startWidth,
             int startHeight) {
-        if (mNoOverrides) return;
         if (isTransitionSet()) {
             clear();
             mNextAppTransitionType = NEXT_TRANSIT_TYPE_SCALE_UP;
@@ -1843,7 +1840,6 @@ public class AppTransition implements Dump {
 
     void overridePendingAppTransitionThumb(GraphicBuffer srcThumb, int startX, int startY,
                                            IRemoteCallback startedCallback, boolean scaleUp) {
-        if (mNoOverrides) return;
         if (isTransitionSet()) {
             clear();
             mNextAppTransitionType = scaleUp ? NEXT_TRANSIT_TYPE_THUMBNAIL_SCALE_UP
@@ -2223,8 +2219,6 @@ public class AppTransition implements Dump {
         for (int i = 0; i < 10; i++) {
             mActivityAnimations[i] = Settings.System.getInt(resolver, Settings.System.ACTIVITY_ANIMATION_CONTROLS[i], 0);
         }
-
-        mNoOverrides = Settings.System.getBoolean(resolver, Settings.System.ANIMATION_CONTROLS_NO_OVERRIDE, false);
 
         int temp = Settings.System.getInt(resolver, Settings.System.ANIMATION_CONTROLS_DURATION, 0);
         mAnimationDuration = temp * 15;
