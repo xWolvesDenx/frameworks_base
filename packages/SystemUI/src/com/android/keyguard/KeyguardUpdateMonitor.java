da08b0b098341f920560d07a160f70cea8b891a5
Kensuke Matsui <kensuke.matsui@sony.com>
2017-03-14 13:27:20 +0900

    Display network name in status bar(1/2)
    When config_showOperatorNameInStatusBar is true, the network name is
    displayed in the status bar.
    The user can turn on/off the feature from Settings app (Settings >
    Display > Network name).
    
    Fixes: 67620513
    Test: manual - insert a valid SIM and go to the Home screen.
    
    Change-Id: I6d03ec00a19e2f5e4c2d2918f1a7c33fdee00dd2

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -1757,6 +1757,10 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
         }
     }
 
+    public ServiceState getServiceState(int subId) {
+        return mServiceStates.get(subId);
+    }
+
     /**
      * @return true if and only if the state has changed for the specified {@code slotId}
      */
