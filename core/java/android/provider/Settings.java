184cfafe81254e6c3b528a35ebcb8fadf0bad2cf
ezio84 <brabus84@gmail.com>
2017-05-11 12:43:29 +0200

    Launch music player on headset connect: add BT compatibility [1/2]
    allow also to exclude carkits, the user might want to start
    android auto or other car audio apps instead.
    
    also move the setting check into the existing SettingsObserver

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3943,6 +3943,12 @@ public final class Settings {
 
         /**
          * Whether to launch default music player when headset plugged in
+         * 0 = don't do anything (default)
+         * 1 = launch only on wired connection
+         * 2 = launch only on bt connection but no carkit
+         * 3 = launch only on bt connection
+         * 4 = launch on both connection types but no carkit
+         * 5 = launch on both connection types
          * @hide
          */
         public static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
@@ -4111,6 +4117,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STATUS_BAR_QUICK_QS_PULLDOWN);
             PRIVATE_SETTINGS.add(VOLUME_BUTTON_MUSIC_CONTROL);
             PRIVATE_SETTINGS.add(LOCKSCREEN_MEDIA_METADATA);
+            PRIVATE_SETTINGS.add(HEADSET_CONNECT_PLAYER);
         }
 
         /**
