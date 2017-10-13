7111fd7c94b664326cb45e1d075d60e5e35f4de1
Marko Man <darkobas@gmail.com>
2017-10-13 16:48:20 +0200

    PiP tile: add toast if pip mode is not avail
    Change-Id: Idfa864c7b26f7965afb233ae377b4a520da326a8

--- core/java/com/android/internal/util/gzosp/GzospUtils.java
+++ core/java/com/android/internal/util/gzosp/GzospUtils.java
@@ -16,8 +16,10 @@
 
 package com.android.internal.util.gzosp;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -43,6 +45,7 @@ import android.view.WindowManager;
 import android.view.IWindowManager;
 import android.view.WindowManagerGlobal;
 
+import java.util.List;
 import java.util.Locale;
 
 import com.android.internal.statusbar.IStatusBarService;
@@ -237,6 +240,22 @@ public class GzospUtils {
                 }
             }
         }
+    }
+
+    public static ActivityInfo getRunningActivityInfo(Context context) {
+        final ActivityManager am = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        final PackageManager pm = context.getPackageManager();
+
+        List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(1);
+        if (tasks != null && !tasks.isEmpty()) {
+            ActivityManager.RunningTaskInfo top = tasks.get(0);
+            try {
+                return pm.getActivityInfo(top.topActivity, 0);
+            } catch (PackageManager.NameNotFoundException e) {
+            }
+        }
+        return null;
     }
 
     // Omni Switch Constants
