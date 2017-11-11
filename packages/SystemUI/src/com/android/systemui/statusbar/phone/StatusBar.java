51d0b7b220130e0b07f77653ac6d28b736a66e3d
ezio84 <brabus84@gmail.com>
2017-11-11 19:25:23 +0100

    Navbar media arrows: more code improvements
    refresh visibility when session is destroyed
    
    Change-Id: I2e4b430da1447997e786d26945aa5ff0313ce055

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -630,6 +630,12 @@ public class StatusBar extends SystemUI implements DemoMode,
             }
             setTrackPlaying();
         }
+
+        @Override
+        public void onSessionDestroyed() {
+            super.onSessionDestroyed();
+            setTrackPlaying();
+        }
     };
 
     private void setTrackPlaying() {
